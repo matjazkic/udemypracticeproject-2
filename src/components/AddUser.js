86b8b70d13024b540dc6576348d135ea7e90b5c6
matjazkic <matjaz.kic@gmail.com>
2023-06-24 14:00:55 +0200

    first commit

---
+++ src/components/AddUser.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./AddUser.module.css";
+
+const AddUser = () => {
+  const submitHandler = () => {};
+
+  return (
+    <div className={styles.input}>
+      <div>
+        <label>Username</label>
+        <input></input>
+      </div>
+      <div>
+        <label>Age (years)</label>
+        <input></input>
+      </div>
+      <button type="submit">Add User</button>
+    </div>
+  );
+};
+
+export default AddUser;
